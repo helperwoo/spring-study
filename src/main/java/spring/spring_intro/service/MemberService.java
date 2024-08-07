00085586b766bf9aa4348213fc6acab0d8a768f1
ko jaewoo <jaewoo.ko@truabutment.com>
2024-08-07 12:57:01 +0900

    회원 서비스 개발

---
+++ src/main/java/spring/spring_intro/service/MemberService.java
@@ -0,0 +1,39 @@
+package spring.spring_intro.service;
+
+import spring.spring_intro.domain.Member;
+import spring.spring_intro.repository.MemberRepository;
+import spring.spring_intro.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
