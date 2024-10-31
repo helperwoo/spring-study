787345909c7380e827bd16cc3dd3f0e1a5c150cd
helperwoo <helperwoo.ko@gmail.com>
2024-10-31 16:18:58 +0900

    스프링 통합 테스트

---
+++ src/test/java/spring/spring_intro/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,36 @@
+package spring.spring_intro.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import spring.spring_intro.domain.Member;
+import spring.spring_intro.repository.MemberRepository;
+import spring.spring_intro.repository.MemoryMemberRepository;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+}
